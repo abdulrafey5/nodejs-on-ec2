35edcbcb11a56e1785e190da2fbb8fb18c4e2c10
abdulrafey5 <160384296+abdulrafey5@users.noreply.github.com>
2024-08-18 15:36:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('You are  in AWS  Cloud. This is my project of deploying nodejs application using aws-ec2, github, jenkins and docker'); //write a response to the client
+  res.write('You are  in AWS  Cloud. This is the project of deploying nodejs application using aws-ec2, github, jenkins and docker'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
